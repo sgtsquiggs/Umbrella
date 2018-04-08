58b89e72e7cf75fb55364c085c2f6bea29be4c0e
Matthew Crenshaw <mcrenshaw@zipari.com>
2018-04-08 13:39:25 -0400

    Umbrella is too simple for my requirements

--- Sources/Umbrella/Umbrella.swift
+++ Sources/Umbrella/Umbrella.swift
@@ -1,34 +1,48 @@
 public protocol AnalyticsType {
-  associatedtype Event: EventType
-  func register(provider: ProviderType)
-  func log(_ event: Event)
+    associatedtype Event: EventType
+    func register(provider: ProviderType)
+    func log(_ event: Event)
 }
 
 public protocol ProviderType {
-  func log(_ eventName: String, parameters: [String: Any]?)
+    func log(_ eventName: String, parameters: [String: Any]?)
+    func identify(parameters: [String: Any])
+    func startSession()
 }
 
 public protocol EventType {
-  func name(for provider: ProviderType) -> String?
-  func parameters(for provider: ProviderType) -> [String: Any]?
+    func name(for provider: ProviderType) -> String?
+    func parameters(for provider: ProviderType) -> [String: Any]?
 }
 
 open class Analytics<Event: EventType>: AnalyticsType {
-  private(set) open var providers: [ProviderType] = []
+    private(set) open var providers: [ProviderType] = []
 
-  public init() {
-    // I'm Analytics 👋
-  }
-
-  open func register(provider: ProviderType) {
-    self.providers.append(provider)
-  }
-
-  open func log(_ event: Event) {
-    for provider in self.providers {
-      guard let eventName = event.name(for: provider) else { continue }
-      let parameters = event.parameters(for: provider)
-      provider.log(eventName, parameters: parameters)
+    public init() {
+        // I'm Analytics 👋
     }
-  }
+
+    open func register(provider: ProviderType) {
+        self.providers.append(provider)
+    }
+
+    open func log(_ event: Event) {
+        for provider in self.providers {
+            guard let eventName = event.name(for: provider) else { continue }
+            let parameters = event.parameters(for: provider)
+            provider.log(eventName, parameters: parameters)
+        }
+    }
+
+    open func identify(parameters: [String: Any]) {
+        for provider in self.providers {
+            provider.identify(parameters: parameters)
+        }
+    }
+
+    open func startSession() {
+        for provider in self.providers {
+            provider.startSession()
+        }
+    }
 }
